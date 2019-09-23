561c5e65fffff886ac8ca44289b2f2a1de51134d
Steven Laff <steve.laff@gmail.com>
2019-09-23 10:53:48 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -14,7 +14,7 @@ class Doctor
     end
 
     def appointments
-        Appointment.all.select { |a| a.doctor == self }
+        Appointment.all.select { |appointment| appointment.doctor == self }
     end
 
     def new_appointment(patient, date)
